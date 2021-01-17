f990ac624976ea79be96049881c22cbf66bac7f7
ishakdolek <ishakdolek@outlook.com>
2021-01-17 21:08:09 +0300

    first commit

--- djCelery/settings.py
+++ djCelery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7*j$ew^p)yuh6mft_zn!pi1=1ei#qm6e^pw7em0kc!1(x$qtf9'
+SECRET_KEY = 'mjdnx5*(46a^8o1y2hztgz1dnjcl@=4qx%i%(&bw@(dh6b722n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_beat'
 ]
 
 MIDDLEWARE = [
--- src/tasks/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery.task.schedules import crontab
-from celery.decorators import periodic_task
-
-
-@periodic_task(run_every=(crontab(minute='1')), name="some_task", ignore_result=True)
-def some_task():
-    print("Hello")
